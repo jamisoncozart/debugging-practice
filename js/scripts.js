d737f1b0812860cc5d32c34fe0e4b5d169a68b00
abe <abeybarra@gmail.com>
2020-01-14 14:22:38 -0800

    add walrus button

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,36 @@
 $(document).ready(function() {
-  $("button#darkLight").click(function() {
-    $("body").removeClass();
-    $("body").addClass("dark-background");
-    //$(".dark-background").toggle();
-  });
+  $("button#hello").click(function() {
+    $("ul#user").prepend("<li>Hello!</li>");
+    $("ul#webpage").prepend("<li>Why hello there!</li>");
+    $("ul#user").children("li").first().click(function() {
+      alert("hi");
+    });
+    $("ul#webpage").children("li").first().click(function() {
+      alert("hi");
+    });
+    $("ul#user").children("li").first().click(function() {
+      $(this).remove();
+    });
+    $("ul#webpage").children("li").first().click(function() {
+      $(this).remove();
+    });
 
-  $("p").click(function(){
-    $(".text-box").toggle();
-    $(".text-noBox").toggle();
-  });
+  
+  $("button#walrus").click(function() {
+    $("ul#user").prepend("<img src='img/walrus.jpg'>");
+    $("ul#webpage").prepend("<li>Walrus Time.</li>");
+    $("ul#user").children("img").first().click(function() {
+      alert("hi");
+    });
+    $("ul#webpage").children("img").first().click(function() {
+      alert("hi");
+    });
+    $("ul#user").children("img").first().click(function() {
+      $(this).remove();
+    });
+    $("ul#webpage").children("img").first().click(function() {
+      $(this).remove();
+    });
+  }); 
+});
 });
